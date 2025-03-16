73ff66806c3d0af8ba6136efd869c56811316b4b
Chadwick Koo <chadwickkoo@outlook.com>
2025-03-16 20:21:28 +0300

    Modified index.js file

--- src/index.js
+++ src/index.js
@@ -6,23 +6,25 @@ document.addEventListener("DOMContentLoaded", () => { // Ensures HTML Structure 
     
     const task = e.target.querySelector("#new-task-description").value;  // Get the input value
 
-    if (!task) return//Exits the function if blank
+    if (!task) return;//Exits the function if blank
 
     handleToDo(task);  
-      form.reset()//Resets form after submitting
+      form.reset();//Resets form after submitting
 });
 });     
 
 // Function to create and delete task
 function handleToDo(todo) {
   let li = document.createElement('li');  // Create a new <li> element
-  let btn = document.createElement('button')//Creates a button
+  let btn = document.createElement('button');//Creates a button
 
-  btn.addEventListener('click', handleDelete)//add an event Listener to the delete button
-  btn.textContent = 'x'
+  btn.addEventListener('click', handleDelete);//add an event Listener to the delete button
+  btn.textContent = 'x';
+
   li.textContent = `${todo  }`;  // Set the text content of the <li> to the task description w/ spaces
-  document.querySelector("#tasks").appendChild(li)  // Append the new <li> element to the <ul>
-  li.appendChild(btn)// Appends a button to the <li> element
+
+  document.querySelector("#tasks").appendChild(li); // Append the new <li> element to the <ul>
+  li.appendChild(btn);// Appends a button to the <li> element
 
 }
 
